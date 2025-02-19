b0e098b0c7cd678a0c00b3eb8fea50c51f4c4dd4
Kanak Rajput <rajputkanak078@gmail.com>
2025-02-19 13:02:31 +0530

    [kanak]Add . added a java object in json file

--- Day2_JSON/src/main/java/com/practiceproblems/convertajavaobject/Car.java
+++ Day2_JSON/src/main/java/com/practiceproblems/convertajavaobject/Car.java
@@ -1,4 +1,18 @@
 package com.practiceproblems.convertajavaobject;
-
+import org.json.JSONObject;
 public class Car {
+
+    public static void main(String[] args) {
+
+
+    JSONObject jobj=new JSONObject();
+        jobj.put("name","Creta");
+        jobj.put("model","EX 1.5 Diesel");
+        jobj.put("color","Black");
+        jobj.put("Type","Fuel");
+        System.out.println(jobj.toString());
+    }
+
+
+
 }
